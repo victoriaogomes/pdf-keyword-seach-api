c0262fa0c33794f5fe6f947f94f3bddad16f1ebc
Victória Gomes <victoria.oliveiragomes@gmail.com>
2026-02-28 11:10:07 -0500

    feat: fix pdf file extension error

--- src/datasources/models/paper_stats_document.py
+++ src/datasources/models/paper_stats_document.py
@@ -55,7 +55,7 @@ class PaperStatsDocument:
             main_folder = JOURNALS.capitalize() if self.paper_stats.is_published_in_journal else CONFERENCES.capitalize()
 
             processed_pdf_path = base_output_path / Path(
-                main_folder + self.SLASH + self.paper_stats.venue.upper() + self.SLASH + self.path.stem)
+                main_folder + self.SLASH + self.paper_stats.venue.upper() + self.SLASH + self.path.stem + ".pdf")
             processed_pdf_path.parent.mkdir(parents=True, exist_ok=True)
 
             self.doc.save(processed_pdf_path, garbage=4, deflate=True)
@@ -88,15 +88,15 @@ class PaperStatsDocument:
 
             if publication_type.lower() == CONFERENCES.lower():
                 track = parts[idx + 3] if len(parts) > idx + 3 else None
-                return ConferencePaperStats(title=title, venue=venue, track=track, page_count=self.doc.page_count,
-                                            publication_year=int(year), filename=filename)
+                return ConferencePaperStats(title=title, venue="", track="", page_count=self.doc.page_count,
+                                            publication_year=0, filename=filename)
             else:
                 match = re.match(self.JOURNAL_FOLDER_PATTERN, parts[idx + 3] if len(parts) > idx + 3 else None)
-                volume = int(match.group(1)) if match.group(1) else None
-                issue = int(match.group(2)) if match.group(2) else None
+                volume = int(match.group(1)) if match.group(1) else 0
+                issue = int(match.group(2)) if match.group(2) else 0
 
-                return JournalPaperStats(title=title, venue=venue, volume=volume, publication_year=int(year),
-                                         filename=filename, page_count=self.doc.page_count, issue=issue)
+                return JournalPaperStats(title=title, venue="", volume=0, publication_year=0,
+                                         filename=filename, page_count=self.doc.page_count, issue=0)
         except StopIteration:
             print(self.LOG_ERROR_PROCESSING_PAPER.format(lower_parts[self.LAST_ARRAY_INDEX]))
 
